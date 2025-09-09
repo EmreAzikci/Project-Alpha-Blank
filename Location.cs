a49c23a082cb82a39589a0580e43587ed2d125ab
EmreAzikci <e.karayilmaz@yahoo.nl>
2025-09-09 11:54:38 +0200

    Added location class

---
+++ Location.cs
@@ -0,0 +1,36 @@
+public class Location
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+
+    // Add these when the classes are done:
+        // public Item ItemRequiredToEnter { get; set; }
+        // public Quest QuestAvailableHere { get; set; }
+        // public Monster MonsterLivingHere { get; set; }
+
+    public Location LocationToNorth { get; set; }
+    public Location LocationToSouth { get; set; }
+    public Location LocationToEast { get; set; }
+    public Location LocationToWest { get; set; }
+
+    public Location(int id, string name, string description)
+        // Add these later into parameters:
+            // Item itemRequiredToEnter = null,
+            // Quest questAvailableHere = null,
+            // Monster monsterLivingHere = null
+    {
+        ID = id;
+        Name = name;
+        Description = description;
+        // add these later:
+            // ItemRequiredToEnter = itemRequiredToEnter;
+            // QuestAvailableHere = questAvailableHere;
+            // MonsterLivingHere = monsterLivingHere;
+    }
+
+    public override string ToString()
+    {
+        return Name;
+    }
+}
