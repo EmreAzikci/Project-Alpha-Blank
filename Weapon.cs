0ca01f853eaf20209c9dbff62a3394397647f6cf
akshaybaldew1 <akkiebaldew@gmail.com>
2025-09-12 11:52:10 +0200

    Added weapon function

--- Weapon.cs
+++ Weapon.cs
@@ -2,3 +2,37 @@
 // ID
 // MaximumDamage
 // Name
+public class Weapon
+{
+    public int ID;
+    public int MaximumDamage;
+    public string Name;
+    public Weapon(int id, int maximumDamage, string name)
+    {
+        ID = id;
+        MaximumDamage = maximumDamage;
+        Name = name;
+    }
+
+    public void DisplayWeapon()
+    {
+        Console.WriteLine($"Weapon ID: {ID}");
+        Console.WriteLine($"Name: {Name}");
+        Console.WriteLine($"Maximum Damage: {MaximumDamage}");
+    }
+
+    public int Attack()
+    {
+        bool isCritical = random.NextDouble() < 0.2;
+
+        if (isCritical)
+        {
+            Console.WriteLine("Critical Hit!");
+            return MaximumDamage * 2;
+        }
+        else
+        {
+            return MaximumDamage;
+        }
+    }
+}
